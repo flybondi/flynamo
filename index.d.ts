1984bd5699dde15a6c055943b0731a2872b1c97f
Nicolás Fantone <nicolasfantone@flybondi.com>
2021-01-20 18:14:54 +0000

    chore: remove extensive docs for batch get item options

--- index.d.ts
+++ index.d.ts
@@ -7,18 +7,9 @@ declare module "@flybondi/flynamo" {
     export type Item = { [key: string]: any };
 
     export interface BatchGetItemInput {
-      /**
-       * Determines the read consistency model: If set to `true`, then the operation uses strongly consistent reads; otherwise, the operation uses eventually consistent reads.
-       */
       ConsistentRead?: DynamoDB.ConsistentRead;
       ReturnConsumedCapacity?: DynamoDB.ReturnConsumedCapacity;
-      /**
-       * A string that identifies one or more attributes to retrieve from the table. These attributes can include scalars, sets, or elements of a JSON document. The attributes in the expression must be separated by commas. If no attribute names are specified, then all attributes are returned. If any of the requested attributes are not found, they do not appear in the result. For more information, see Specifying Item Attributes in the Amazon DynamoDB Developer Guide.
-       */
       ProjectionExpression?: DynamoDB.ProjectionExpression;
-      /**
-       * One or more substitution tokens for attribute names in an expression. The following are some use cases for using ExpressionAttributeNames:   To access an attribute whose name conflicts with a DynamoDB reserved word.   To create a placeholder for repeating occurrences of an attribute name in an expression.   To prevent special characters in an attribute name from being misinterpreted in an expression.   Use the # character in an expression to dereference an attribute name. For example, consider the following attribute name:    Percentile    The name of this attribute conflicts with a reserved word, so it cannot be used directly in an expression. (For the complete list of reserved words, see Reserved Words in the Amazon DynamoDB Developer Guide). To work around this, you could specify the following for ExpressionAttributeNames:    {"#P":"Percentile"}    You could then use this substitution in an expression, as in this example:    #P = :val     Tokens that begin with the : character are expression attribute values, which are placeholders for the actual value at runtime.  For more information on expression attribute names, see Specifying Item Attributes in the Amazon DynamoDB Developer Guide.
-       */
       ExpressionAttributeNames?: DynamoDB.ExpressionAttributeNameMap;
     }
 
