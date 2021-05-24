18ecb2597f137f01cbf59a8d386f843b9024475b
Luciano Fantone <lfantone@users.noreply.github.com>
2021-05-24 08:36:08 +0200

    fix: improve type definitions (#36)
    * chore: bump dependencies
    
    * chore: improve type definitions
    
    * chore: add types for default methods

--- index.d.ts
+++ index.d.ts
@@ -1,8 +1,8 @@
 import { DynamoDB } from 'aws-sdk';
 import DynamoDBWrapper from 'dynamodb-wrapper';
 
-declare module "@flybondi/flynamo" {
-  module Flynamo {
+declare module '@flybondi/flynamo' {
+  namespace Flynamo {
     export type KeyAttributes = { [key: string]: string | number };
     export type Key = string | number | KeyAttributes | { Key: KeyAttributes };
     export type Item = { [key: string]: any };
@@ -17,19 +17,48 @@ declare module "@flybondi/flynamo" {
     export type BatchWriteItemInput = Omit<DynamoDB.BatchWriteItemInput, 'RequestItems'>;
 
     export interface BatchWriteItemOperations {
-      insert?: Item | Item[],
-      remove?: Key | Key[]
+      insert?: Item | Item[];
+      remove?: Key | Key[];
     }
 
-    export type GetItemInput = Omit<DynamoDB.GetItemInput, 'TableName' | 'Key' | 'AttributesToGet'>;
+    interface IGetItemInput extends DynamoDB.GetItemInput {
+      TableName?: string;
+    }
 
-    export type PutItemInput = Omit<DynamoDB.PutItemInput, 'TableName' | 'Item' | 'Expected' | 'ConditionalOperator'>;
+    export type GetItemInput = Omit<IGetItemInput, 'Key' | 'AttributesToGet'>;
 
-    export type QueryInput = Omit<DynamoDB.QueryInput, 'TableName' | 'AttributesToGet' | 'KeyConditions' | 'QueryFilter' | 'ConditionalOperator'>;
+    interface IPutItemInput extends DynamoDB.PutItemInput {
+      TableName?: string;
+    }
 
-    export type DeleteItemInput = Omit<DynamoDB.DeleteItemInput, 'TableName' | 'Key' | 'Expected' | 'ConditionalOperator'>;
+    export type PutItemInput = Omit<IPutItemInput, 'Item' | 'Expected' | 'ConditionalOperator'>;
 
-    export type UpdateItemInput = Omit<DynamoDB.UpdateItemInput, 'TableName' | 'Key' | 'AttributeUpdates' | 'Expected' | 'ConditionalOperator'>;
+    interface IQueryInput extends DynamoDB.QueryInput {
+      TableName?: string;
+    }
+
+    export type QueryInput = Omit<
+      IQueryInput,
+      'AttributesToGet' | 'KeyConditions' | 'QueryFilter' | 'ConditionalOperator'
+    >;
+
+    interface IDeleteItemInput extends DynamoDB.DeleteItemInput {
+      TableName?: string;
+    }
+
+    export type DeleteItemInput = Omit<
+      IDeleteItemInput,
+      'Key' | 'Expected' | 'ConditionalOperator'
+    >;
+
+    interface IUpdateItemInput extends DynamoDB.UpdateItemInput {
+      TableName?: string;
+    }
+
+    export type UpdateItemInput = Omit<
+      IUpdateItemInput,
+      'Key' | 'AttributeUpdates' | 'Expected' | 'ConditionalOperator'
+    >;
 
     export interface QueryOptions extends DynamoDBWrapper.IQueryOptions {
       /**
@@ -38,10 +67,164 @@ declare module "@flybondi/flynamo" {
       raw?: boolean;
     }
 
-    type UpdateExpressionBuilderInput = Pick<DynamoDB.Update, 'UpdateExpression' | 'ExpressionAttributeNames' | 'ExpressionAttributeValues'>;
-    export type UpdateExpressionBuilder = (input: UpdateExpressionBuilderInput) => DynamoDB.ExpressionAttributeValueMap;
+    type UpdateExpressionBuilderInput = Pick<
+      DynamoDB.Update,
+      'UpdateExpression' | 'ExpressionAttributeNames' | 'ExpressionAttributeValues'
+    >;
+    export type UpdateExpressionBuilder = (
+      input: UpdateExpressionBuilderInput
+    ) => DynamoDB.ExpressionAttributeValueMap;
   }
-  export interface Flynamo {
+
+  export interface FlynamoClient {
+    /**
+     * Return the number of elements in a table or a secondary index. This function
+     * uses `Scan` internally and retrieves the returned `Count` attribute from its response.
+     *
+     * @example
+     *
+     * // Returns the total count of items from a table
+     * await count();
+     *
+     * @param request - Parameters as expected by DynamoDB `Scan` operation.
+     * @returns A `Promise` that resolves to the total number of elements
+     */
+    count: (request: DynamoDB.ScanInput) => Promise<DynamoDB.Integer>;
+
+    /**
+     * Returns a set of attributes for the item with the given primary key.
+     * If there is no matching item, it returns `undefined`. The name of the `key` defaults
+     * to `id` if it's not specified. The `Key` value (as expected by a `AWS.DynamoDB` regular request)
+     * can be omitted (i.e.: `{ id: 42 }` is equivalent to `{ Key: { id: 42 } }`).
+     *
+     * @example
+     *
+     *  // Return the item with a primary key of `{ pk: 'foo', sk: 'bar#baz' }`
+     *  await get({ pk: 'foo', sk: 'bar#baz' });
+     *
+     * @example
+     *
+     *  // Returns the item with a primary key of `{ id: 42 }`
+     *  await get({ Key: { id: 42 } });
+     *
+     * @param key - The primary key value.
+     * @param input - Parameters as supported by `AWS.`DynamoDB` `GetItem` operation. Any `Key` attribute
+     *  specified here will override primary key value in `key`.
+     * @returns A `Promise` that resolves to the item returned by `AWS.DynamoDB` response or `undefined` if it
+     *  does not exist.
+     */
+    get: <R extends Flynamo.Item>(
+      key: Flynamo.Key,
+      input: Flynamo.GetItemInput
+    ) => Promise<R | void>;
+
+    /**
+     * Returns all items in a table or a secondary index. This uses `Scan` internally.
+     *
+     * @example
+     *
+     *  // Return all items in a table
+     *  await getAll({ TableName: 'table-name' });
+     *
+     * @param input - Parameters as expected by `AWS.DynamoDB` `Scan` operation.
+     * @returns A `Promise` that resolves to an array of `Items` returned by the `AWS.DynamoDB` response.
+     */
+    getAll: <R extends Flynamo.Item[]>(input: DynamoDB.ScanInput) => Promise<R>;
+
+    /**
+     * Creates a new item, or replaces an old item with a new item.
+     * If an item that has the same primary key as the new item already exists in the table,
+     * the new item completely replaces the existing item. This function uses `PutItem` internally.
+     *
+     * @example
+     *
+     *  // Insert or update item of primary key `{id: 42}`
+     *  await insert({ id: 42, foo: 'bar' }, { TableName: 'table-name' });
+     *
+     *
+     * @param item - The item to insert. Will be converted into a map of `AWS.DynamoDB` attributes internally.
+     * @param input - Parameters as expected by `AWS.DynamoDB` `PutItem` operation.
+     * @returns  Resolves to the response from DynamoDB client.
+     */
+    insert: (item: Flynamo.Item, input: Flynamo.PutItemInput) => Promise<DynamoDB.PutItemOutput>;
+
+    /**
+     * Finds items based on primary key values.
+     *
+     * @example
+     *
+     *  // Returns all items of `pk` `42` and `birthDate` between 1985 and 2020
+     *  await query({
+     *    TableName: 'table-name',
+     *    KeyConditionExpression: "pk = :pk AND birthDate BETWEEN :start AND :end",
+     *    ExpressionAttributeValues: {
+     *      ':pk': { N: 42 },
+     *      ':start': { S: '1985-01-01' },
+     *      ':end': { S: '2020-01-01' }
+     *    }
+     *  });
+     *
+     * @param input - Parameters as expected by `AWS.DynamoDB` `Query` operation.
+     * @param options - Configuration options parameters.
+     * @returns A `Promise` that resolves to either an array of returned `Items` or the full, raw response from `AWS.DynamoDB`.
+     */
+    query: <R extends Flynamo.Item>(
+      input: Flynamo.QueryInput,
+      options?: Flynamo.QueryOptions
+    ) => Promise<Array<R>>;
+    query: (
+      input: Flynamo.QueryInput,
+      options: Omit<Flynamo.QueryOptions, 'raw'> & { raw: boolean }
+    ) => Promise<DynamoDB.QueryOutput>;
+
+    /**
+     * Deletes a single item in a table by primary key. Returns the recently removed item by default
+     * (the `Attributes` value present in the `AWS.DynamoDB` response after having set `ReturnValues` parameter to `ALL_OLD`).
+     * This function uses `DeleteItem` operation internally.
+     *
+     * @example
+     *
+     *  // Deletes an item of primary key `{ id: 42 }`
+     *  await remove(42, { TableName: 'table-name' });
+     *
+     * @param key - The primary key value of the item to delete.
+     * @param input - Parameters as expected by `AWS.DynamoDB` `DeleteItem` operation.
+     * @returns A `Promise` that resolves to the `Attributes` property of the DynamoDB response. Unless, `ReturnValues` has
+     *  been explicitly set, this will match all attributes of the recently deleted element.
+     */
+    remove: (key: Flynamo.Key, input: Flynamo.DeleteItemInput) => Promise<DynamoDB.AttributeMap>;
+    remove: <R extends Flynamo.Item>(
+      key: Flynamo.Key,
+      input: Omit<Flynamo.DeleteItemInput, 'ReturnValues'> & { ReturnValues: string }
+    ) => Promise<R>;
+
+    /**
+     * Edits an existing item's attributes, or adds a new item to the table if it does not
+     * already exist by its primary key. The primary key name defaults to `id` if not explicitly provided.
+     * Returns the `Attributes` value from the `AWS.DynamoDB` response. By default, it sets `ReturnValues` to `ALL_NEW`
+     * so it returns all of the attributes of the item, as they appear after the update operation. This function
+     * uses the `UpdateItem` operation internally.
+     *
+     * @example
+     *
+     *  // Updates item with primary key `{ id: 42 }`
+     *  await update(42, { foo: 'bar' }, { TableName: 'table-name' });
+     *
+     * @param key - The primary key value.
+     * @param itemOrBuilder - Either an update expression builder function or the partial item that will be merged with the existing item in `AWS.DynamoDB`. An appropriate
+     *  `UpdateExpression` will be automatically created from this argument. While you can `null` them or replace them,
+     *  removing attributes from an item is only supported through manually defining an `UpdateExpression` using the `input` argument.
+     * @param request - Parameters as expected by `AWS.DynamoDB` `UpdateItem` operation. Setting an `UpdateExpression` value here
+     *  will override any value automatically derived from `itemOrBuilder`.
+     * @returns A `Promise` that resolves to the `Attributes` property of the `AWS.DynamoDB` response.
+     */
+    update: <R extends Flynamo.Item>(
+      key: Flynamo.Key,
+      itemOrBuilder: Flynamo.Item | Flynamo.UpdateExpressionBuilder,
+      request: Flynamo.UpdateItemInput
+    ) => Promise<R>;
+  }
+  export interface ForFlynamoClient {
     /**
      * Returns the attributes of one or more items from a table in a single request.
      * Requested items are identified by primary key. A key represented by simply a `number` or `string`
@@ -66,7 +249,10 @@ declare module "@flybondi/flynamo" {
      * @param input - Optional settings supported by `AWS.DynamoDB` for this operation.
      * @returns Resolves to the response from `AWS.DynamoDB` client for a `BatchGetItem` operation.
      */
-    batchGet: (keys: Flynamo.Key | Flynamo.Key[], input?: Flynamo.BatchGetItemInput) => Promise<DynamoDB.BatchGetItemOutput>,
+    batchGet: (
+      keys: Flynamo.Key | Flynamo.Key[],
+      input?: Flynamo.BatchGetItemInput
+    ) => Promise<DynamoDB.BatchGetItemOutput>;
 
     /**
      * Puts (i.e.: inserts) items in a table in a single batch.
@@ -81,7 +267,10 @@ declare module "@flybondi/flynamo" {
      * @param input - Optional settings supported by `AWS.DynamoDB` for this operation.
      * @returns Resolves to the response from `AWS.DynamoDB` client for a `BatchWriteItem` operation.
      */
-    batchInsert: (items: Flynamo.Item | Flynamo.Item[], input?: Flynamo.BatchWriteItemInput) => Promise<DynamoDB.BatchWriteItemOutput>,
+    batchInsert: (
+      items: Flynamo.Item | Flynamo.Item[],
+      input?: Flynamo.BatchWriteItemInput
+    ) => Promise<DynamoDB.BatchWriteItemOutput>;
 
     /**
      * Deletes multiple items from a table in a single batch.
@@ -102,8 +291,10 @@ declare module "@flybondi/flynamo" {
      * @param input - Optional settings supported by `AWS.DynamoDB` for this operation.
      * @returns Resolves to the response from `AWS.DynamoDB` client for a `BatchWriteItem` operation.
      */
-    batchRemove: (keys: Flynamo.Key | Flynamo.Key[], input?: Flynamo.BatchWriteItemInput) => Promise<DynamoDB.BatchWriteItemOutput>,
-
+    batchRemove: (
+      keys: Flynamo.Key | Flynamo.Key[],
+      input?: Flynamo.BatchWriteItemInput
+    ) => Promise<DynamoDB.BatchWriteItemOutput>;
 
     /**
      * Puts (i.e.: inserts) or deletes multiple items in a table in a single operation. It expects an
@@ -124,7 +315,10 @@ declare module "@flybondi/flynamo" {
      * @param operations - An object containing `insert` and/or `remove` properties.
      * @returns - Resolves to the response from `AWS.DynamoDB` client for a `BatchWriteItem` operation.
      */
-    batchWrite: (operations: Flynamo.BatchWriteItemOperations, input?: Flynamo.BatchWriteItemInput) => Promise<DynamoDB.BatchWriteItemOutput>,
+    batchWrite: (
+      operations: Flynamo.BatchWriteItemOperations,
+      input?: Flynamo.BatchWriteItemInput
+    ) => Promise<DynamoDB.BatchWriteItemOutput>;
 
     /**
      * Return the number of elements in a table or a secondary index. This function
@@ -138,7 +332,7 @@ declare module "@flybondi/flynamo" {
      * @param request - Parameters as expected by DynamoDB `Scan` operation.
      * @returns A `Promise` that resolves to the total number of elements
      */
-    count: () => Promise<DynamoDB.Integer>,
+    count: (request?: DynamoDB.ScanInput) => Promise<DynamoDB.Integer>;
 
     /**
      * Returns a set of attributes for the item with the given primary key.
@@ -167,7 +361,10 @@ declare module "@flybondi/flynamo" {
      * @returns A `Promise` that resolves to the item returned by `AWS.DynamoDB` response or `undefined` if it
      *  does not exist.
      */
-    get: (key: Flynamo.Key, input?: Flynamo.GetItemInput) => Promise<Flynamo.Item[]>,
+    get: <R extends Flynamo.Item>(
+      key: Flynamo.Key,
+      input?: Flynamo.GetItemInput
+    ) => Promise<R | void>;
 
     /**
      * Returns all items in a table or a secondary index. This uses `Scan` internally.
@@ -180,7 +377,7 @@ declare module "@flybondi/flynamo" {
      * @param input - Parameters as expected by `AWS.DynamoDB` `Scan` operation.
      * @returns A `Promise` that resolves to an array of `Items` returned by the `AWS.DynamoDB` response.
      */
-    getAll: (input?: DynamoDB.ScanInput) => Promise<Flynamo.Item[]>,
+    getAll: <R extends Flynamo.Item[]>(input?: DynamoDB.ScanInput) => Promise<R>;
 
     /**
      * Creates a new item, or replaces an old item with a new item.
@@ -197,7 +394,7 @@ declare module "@flybondi/flynamo" {
      * @param input - Optional parameters as expected by `AWS.DynamoDB` `PutItem` operation.
      * @returns  Resolves to the response from DynamoDB client.
      */
-    insert: (item: Flynamo.Item, input?: Flynamo.PutItemInput) => Promise<DynamoDB.PutItemOutput>,
+    insert: (item: Flynamo.Item, input?: Flynamo.PutItemInput) => Promise<DynamoDB.PutItemOutput>;
 
     /**
      * Finds items based on primary key values.
@@ -218,7 +415,14 @@ declare module "@flybondi/flynamo" {
      * @param options - Configuration options parameters.
      * @returns A `Promise` that resolves to either an array of returned `Items` or the full, raw response from `AWS.DynamoDB`.
      */
-    query: (input: Flynamo.QueryInput, options?: Flynamo.QueryOptions) => Promise<DynamoDB.QueryOutput | Item[]>,
+    query: <R extends Flynamo.Item>(
+      input: Flynamo.QueryInput,
+      options?: Flynamo.QueryOptions
+    ) => Promise<Array<R>>;
+    query: (
+      input: Flynamo.QueryInput,
+      options: Omit<Flynamo.QueryOptions, 'raw'> & { raw: boolean }
+    ) => Promise<DynamoDB.QueryOutput>;
 
     /**
      * Deletes a single item in a table by primary key. Returns the recently removed item by default
@@ -235,7 +439,11 @@ declare module "@flybondi/flynamo" {
      * @returns A `Promise` that resolves to the `Attributes` property of the DynamoDB response. Unless, `ReturnValues` has
      *  been explicitly set, this will match all attributes of the recently deleted element.
      */
-    remove: (key: Flynamo.Key, input?: Flynamo.DeleteItemInput) => Promise<Flynamo.Item>,
+    remove: (key: Flynamo.Key, input?: Flynamo.DeleteItemInput) => Promise<DynamoDB.AttributeMap>;
+    remove: <R extends Flynamo.Item>(
+      key: Flynamo.Key,
+      input: Omit<Flynamo.DeleteItemInput, 'ReturnValues'> & { ReturnValues: string }
+    ) => Promise<R>;
 
     /**
      * Edits an existing item's attributes, or adds a new item to the table if it does not
@@ -253,14 +461,18 @@ declare module "@flybondi/flynamo" {
      * @param itemOrBuilder - Either an update expression builder function or the partial item that will be merged with the existing item in `AWS.DynamoDB`. An appropriate
      *  `UpdateExpression` will be automatically created from this argument. While you can `null` them or replace them,
      *  removing attributes from an item is only supported through manually defining an `UpdateExpression` using the `input` argument.
-     * @param input - Optional parameters as expected by `AWS.DynamoDB` `UpdateItem` operation. Setting an `UpdateExpression` value here
+     * @param request - Optional parameters as expected by `AWS.DynamoDB` `UpdateItem` operation. Setting an `UpdateExpression` value here
      *  will override any value automatically derived from `itemOrBuilder`.
      * @returns A `Promise` that resolves to the `Attributes` property of the `AWS.DynamoDB` response.
      */
-    update: (key: Flynamo.Key, itemOrBuilder: Flynamo.Item | Flynamo.UpdateExpressionBuilder, input?: Flynamo.UpdateItemInput) => Promise<Flynamo.Item>
+    update: <R extends Flynamo.Item>(
+      key: Flynamo.Key,
+      itemOrBuilder: Flynamo.Item | Flynamo.UpdateExpressionBuilder,
+      request?: Flynamo.UpdateItemInput
+    ) => Promise<R>;
   }
 
-  export interface FlynamoClient {
+  export interface ForTable {
     /**
      * Returns a `Flynamo` API that automatically adds a `TableName` prop
      * to all its requests.
@@ -274,7 +486,7 @@ declare module "@flybondi/flynamo" {
      * @param tableName - The value of `TableName`.
      * @returns The entire `Flynamo` scoped to a single table.
      */
-    forTable(tableName: string): Flynamo
+    forTable(tableName: string): ForFlynamoClient;
   }
 
   /**
@@ -285,8 +497,11 @@ declare module "@flybondi/flynamo" {
    * @see https://docs.aws.amazon.com/AWSJavaScriptSDK/latest/AWS/DynamoDB.html
    * @param client - A `AWS.DynamoDB` client.
    * @param options - `DynamoDBWrapper` configuration (optional).
-  */
-  export declare function flynamo(dynamodb: DynamoDB, options?: DynamoDBWrapper.IDynamoDBWrapperOptions): FlynamoClient;
+   */
+  export declare function flynamo(
+    dynamodb: DynamoDB,
+    options?: DynamoDBWrapper.IDynamoDBWrapperOptions
+  ): FlynamoClient & ForTable;
 
   export = flynamo;
 }
